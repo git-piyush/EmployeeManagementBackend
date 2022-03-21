535f9ea96772c6a0a95e803ed0e246c1d9c7f267
Piyush kumar <kmrpiyush95@gmail.com.com>
2022-03-21 17:16:20 +0530

    adding file upload functionalities

--- src/main/java/com/example/demo/controller/AttachmentController.java
+++ src/main/java/com/example/demo/controller/AttachmentController.java
@@ -6,9 +6,11 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,35 +20,38 @@ import com.example.demo.dto.ResponseData;
 import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 
-
+@CrossOrigin(origins = "https://em-frontend.herokuapp.com/")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/v1/")
 public class AttachmentController {
-	
+
 	@Autowired
 	EmployeeService employeeService;
 
-	@PutMapping("/upload/{id}")
+	@PutMapping("/piyush/upload/{id}")
 	public ResponseData uploadFile(@RequestParam("file") MultipartFile file, @PathVariable Long id) throws Exception {
 		System.out.println("Hello");
 		Employee employee = null;
 		String downloadURl = "";
 		employee = employeeService.saveAttachment(file, id);
 		String empId = String.valueOf(employee.getId());
-		
-		downloadURl = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/").path(empId)
-				.toUriString();
 
-		return new ResponseData(employee.getAttachment().getFileName(), downloadURl, file.getContentType(), file.getSize());
+		downloadURl = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/").path(empId).toUriString();
+
+		return new ResponseData(employee.getAttachment().getFileName(), downloadURl, file.getContentType(),
+				file.getSize());
 	}
-	
-	
-	@GetMapping("/download/{empId}")
+
+	@GetMapping("/piyush/download/{empId}")
 	public ResponseEntity<Resource> downloadFile(@PathVariable Long empId) throws Exception {
 		Employee employee = null;
 		employee = employeeService.getAttachment(empId);
+
 		return ResponseEntity.ok().contentType(MediaType.parseMediaType(employee.getAttachment().getFileType()))
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + employee.getAttachment().getFileName() + "\"")
 				.body(new ByteArrayResource(employee.getAttachment().getData()));
+
 	}
-	
+
 }
